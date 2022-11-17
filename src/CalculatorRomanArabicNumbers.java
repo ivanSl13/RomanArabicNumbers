cc0ef5cb5e6d819af5f8a2ac3257da3753ffeace
ivanSl13 <natmab@yandex.ru>
2022-11-17 20:43:30 +0300

    Delite 102

--- src/CalculatorRomanArabicNumbers.java
+++ src/CalculatorRomanArabicNumbers.java
@@ -85,6 +85,7 @@ public class CalculatorRomanArabicNumbers {
         List<RomanNumer> romanNumerals = RomanNumer.getReverseSortedValues();
         int i = 0;
         StringBuilder sb = new StringBuilder();
+        System.out.println(sb);
         while ((number > 0) && (i < romanNumerals.size())) {
             RomanNumer currentSymbol = romanNumerals.get(i);
             if (currentSymbol.getValue() <= number) {
@@ -98,8 +99,7 @@ public class CalculatorRomanArabicNumbers {
     }
     public enum RomanNumer {
         I(1), IV(4), V(5), IX(9), X(10),
-        XL(40), L(50), XC(90), C(100),
-        CD(400), D(500), CM(900), M(1000);
+        XL(40), L(50), XC(90), C(100);
         private int value;
         RomanNumer(int value) {
             this.value = value;
